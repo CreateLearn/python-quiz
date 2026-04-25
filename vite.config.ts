0a84a1474d50e8ff2d73b158083649203a2e2115
Yuanzong Qiu <congge@gmail.com>
2026-04-25 11:55:15 -0400

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   base: process.env.BASE_PATH ?? '/',
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          mui: ['@mui/material', '@mui/icons-material'],
-          react: ['react', 'react-dom'],
-          sanitizer: ['dompurify', 'zod'],
+        manualChunks(id) {
+          if (id.includes('node_modules/@mui/')) {
+            return 'mui';
+          }
+          if (id.includes('node_modules/react')) {
+            return 'react';
+          }
+          if (
+            id.includes('node_modules/dompurify') ||
+            id.includes('node_modules/zod')
+          ) {
+            return 'sanitizer';
+          }
+          return undefined;
         },
       },
     },
